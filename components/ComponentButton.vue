97f2b3bdc71061e803de3e238985aef9d7187150
axel <notevil@mail.tu>
2020-07-09 00:09:11 +0300

    task completed

---
+++ components/ComponentButton.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <button @click="getMessage" class="btn-color">{{name}}</button>
+</div>
+</template>
+
+<script>
+export default {
+props: {
+    name:{
+      type: String,
+      required: String,
+    }
+    
+},
+methods: {
+    getMessage(){
+        this.$emit("alerter")
+    }
+}
+}
+</script>
+
+<style scoped>
+.btn-color{
+    width: 100px;
+    height: 40px;
+    margin-top: 10px;
+    background: white;
+    border: solid 1px green;
+}
+</style>
