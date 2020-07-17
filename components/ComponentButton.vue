19875f4293f9a31e424971eb98a1b901c2e81d68
axel <notevil@mail.tu>
2020-07-17 15:57:06 +0300

    makedButton

--- components/ComponentButton.vue
+++ components/ComponentButton.vue
@@ -1,12 +1,11 @@
 <template>
-   <div >
-    <button @click="getMessage" class="btn">{{name}}</button>
-   </div>
+  <div>
+    <button @click="getMessage()" class="btn">{{name}}</button>
+  </div>
 </template>
 
 <script>
 export default {
-
   props: {
     name: {
       type: String,
@@ -16,53 +15,55 @@ export default {
       type: String,
       default: 'randomNumber',
     },
-
   },
   data() {
     return {
       count: 0,
       asciiKeys: [],
-      init: true,
     };
   },
-
+  computed: {
+    somemethod3() {
+      const mass = [];
+      for (let i = 65; i <= 122; i += 1) {
+        if (i < 91 || i > 96) {
+          mass.push(i);
+        }
+      }
+      return mass;
+    },
+  },
   methods: {
     getMessage() {
+      let result;
       switch (this.actionChoose) {
-        case 'randomNumber': this.$emit('clicked', (Math.floor(Math.random() * 100) + 1));
+        case 'randomNumber':
+          result = Math.floor(Math.random() * 100) + 1;
           break;
         case 'Counter':
-          this.$emit('clicked', this.count += 1);
+          this.count += 1;
+          result = this.count;
           break;
         case 'randomABC':
-          if (this.init) {
-            this.initArray();
-          }
-          this.$emit('clicked', String.fromCharCode(this.asciiKeys[
-            Math.floor(Math.random() * this.asciiKeys.length)]));
+          result = String.fromCharCode(this.somemethod3[
+            Math.floor(Math.random() * this.somemethod3.length)]);
           break;
         default:
           break;
       }
-    },
-    initArray() {
-      for (let i = 65; i <= 122; i += 1) {
-        if (i < 91 || i > 96) {
-          this.asciiKeys.push(i);
-        }
-      }
-      this.init = false;
+
+      this.$emit('clicked', result);
     },
   },
 };
 </script>
 
 <style scoped>
-.btn{
-    width: 100px;
-    height: 40px;
-    margin-top: 10px;
-    background: white;
-    border: solid 1px green;
+.btn {
+  width: 100px;
+  height: 40px;
+  margin-top: 10px;
+  background: white;
+  border: solid 1px green;
 }
 </style>
